34f134bc26efb6ae459c2953858ffd5ab4aa426b
Wei Jiang <weijiang@Weis-MacBook-Pro.local>
2018-04-21 13:19:06 -0400

    vs

---
+++ vs.cpp
@@ -0,0 +1,33 @@
+//Atropos game AI
+//BU CS 440/640 2018 Spring
+//Wei Jiang and Yitian Lin
+
+//compile command: make
+//run command: 
+
+#include <iostream>
+#include "tree_node.h"
+#include "board.h"
+
+using namespace std;
+
+int main (int argc, char *argv[])
+{
+    cout << argv[1] << endl;
+    TreeNode test_tn2 = TreeNode(argv[1], 0, nullptr);
+
+    cout << endl;     
+
+    ab_dfs(&test_tn2);
+
+    cout << "root node score provider is:" << endl;
+    printScorePath(&test_tn2);
+    cout << endl;
+    printOutput(&test_tn2);
+    cout << "ab_dfs times: " << ab_counter << endl;
+
+    //test_tn.score_provider->printNode();
+    //test_b.printMoves();
+
+    return 0;
+}
